ce80b1c5a2e5261f66b796ed79d5ab9a7ffdeb36
_NBX_ <104787483+NicoleBroadnax@users.noreply.github.com>
2024-02-28 10:29:18 -0500

    Initial React Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+function MyComponent() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get(
+          "https://services6.arcgis.com/bdPqSfflsdgFRVVM/arcgis/rest/services/Trash_Day_Schedule/FeatureServer/2/query?where=1%3D1&outFields=StName&outSR=4326&f=json"
+        );
+        setData(response.data);
+      } catch (error) {
+        console.error("There was an error fetching the data:", error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>FINAL VERSION</h1>
+      {JSON.stringify(data.features.attributes.StName)}
+      {/* {data ? JSON.stringify(data.features.attributes.StName) : "Loading..."} */}
     </div>
   );
 }
 
-export default App;
+export default MyComponent;
