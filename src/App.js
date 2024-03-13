8ae678963b106e360595f92c97d82c33baee4af5
_NBX_ <104787483+NicoleBroadnax@users.noreply.github.com>
2024-03-12 20:20:44 -0400

    update trash search

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-function MyComponent() {
+function TrashDaySearch() {
   const [data, setData] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     async function fetchData() {
       try {
         const response = await axios.get(
-          "https://services6.arcgis.com/bdPqSfflsdgFRVVM/arcgis/rest/services/Trash_Day_Schedule/FeatureServer/2/query?where=1%3D1&outFields=StName&outSR=4326&f=json"
+          "https://services6.arcgis.com/bdPqSfflsdgFRVVM/arcgis/rest/services/Trash_Day_Schedule/FeatureServer/2/query?where=1%3D1&outFields=StNum,StName,FullAddress,Zip,TrashDay&outSR=4326&f=json"
         );
         setData(response.data);
       } catch (error) {
@@ -19,15 +20,55 @@ function MyComponent() {
     fetchData();
   }, []);
 
-  return (
-    <div>
-      <h1>This is the final version</h1>
-      {JSON.stringify(data.features.attributes.StName)}
+  const filterData = data
+    ? data.features.filter((feature) => {
+        if (!feature.attributes?.StName) return false;
+        const address = feature.attributes.StName.toLowerCase();
+        const date = feature.attributes?.TrashDay
+          ? feature.attributes?.TrashDay.toLowerCase()
+          : "";
 
-      <div></div>
-      {/* {data ? JSON.stringify(data.features.attributes.StName) : "Loading..."} */}
+        return (
+          address.includes(searchTerm.toLowerCase()) ||
+          date.includes(searchTerm.toLowerCase())
+        );
+      })
+    : [];
+
+  return (
+    <div className="container">
+      <input
+        type="text"
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      {filterData.length > 0 && searchTerm ? (
+        <table className="table table-bordered table-hover">
+          <thead>
+            <tr>
+              <th scope="col">House Number</th>
+              <th scope="col">Street Name</th>
+              <th scope="col">TrashDay</th>
+              <th scope="col">ZipCode</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filterData.map((feature) => (
+              <tr key={feature.attributes.OBJECTID}>
+                <td>{feature.attributes.StNum}</td>
+                <td>{feature.attributes.StName}</td>
+                <td>{feature.attributes.TrashDay}</td>
+                <td>{feature.attributes.Zip}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p>no results found.</p>
+      )}
     </div>
   );
 }
 
-export default MyComponent;
+export default TrashDaySearch;
