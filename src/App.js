37d806e4ffc43f24cccc8e6d0ea5cfd7725a2719
_NBX_ <104787483+NicoleBroadnax@users.noreply.github.com>
2024-04-19 09:56:24 -0400

    updated css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import "./App.css";
 
 function TrashDaySearch() {
   const [data, setData] = useState(null);
   const [searchTerm, setSearchTerm] = useState("");
+  //const [ZipCode, setZipcode]=useState("");
 
   useEffect(() => {
     async function fetchData() {
@@ -28,47 +30,79 @@ function TrashDaySearch() {
           ? feature.attributes?.TrashDay.toLowerCase()
           : "";
 
+        const zip = feature.attributes?.Zip
+          ? feature.attributes?.Zip.toString()
+          : "";
+
         return (
           address.includes(searchTerm.toLowerCase()) ||
-          date.includes(searchTerm.toLowerCase())
+          date.includes(searchTerm.toLowerCase()) ||
+          zip.includes(searchTerm)
         );
       })
     : [];
 
   return (
-    <div className="container">
-      <input
-        type="text"
-        placeholder="Search..."
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
-      {filterData.length > 0 && searchTerm ? (
-        <table className="table table-bordered table-hover">
-          <thead>
-            <tr>
-              <th scope="col">House Number</th>
-              <th scope="col">Street Name</th>
-              <th scope="col">TrashDay</th>
-              <th scope="col">ZipCode</th>
-            </tr>
-          </thead>
-          <tbody>
-            {filterData.map((feature) => (
-              <tr key={feature.attributes.OBJECTID}>
-                <td>{feature.attributes.StNum}</td>
-                <td>{feature.attributes.StName}</td>
-                <td>{feature.attributes.TrashDay}</td>
-                <td>{feature.attributes.Zip}</td>
+    <div className="container.max-width">
+      <nav className="navbar">
+        <img
+          className="syracuseLogowhite.png"
+          src="SyracuseLogowhite.png"
+          alt="Logo"
+          width="225"
+          padding-left="25"
+          padding-right="0px"
+        ></img>
+        <ul className=" nav justify-content-end">
+          <li className="nav-item"></li>
+          <li className="nav-item"></li>
+          <li className="nav-item"></li>
+          <li className="nav-item"></li>
+        </ul>
+      </nav>
+      <div className="title">Find Property Trash Day Schedule</div>
+      <body />
+      <div classname="container">
+        <input
+          className="searchInput"
+          type="text"
+          placeholder="Search..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        {filterData.length > 0 && searchTerm ? (
+          <table
+            className="custom-table 
+           table table-bordered table-hover --bs-table-accent-bg"
+          >
+            <thead className="custom-table2 table-warning table-border-solid">
+              <tr>
+                <th scope="col">House Number</th>
+                <th scope="col">Street Name</th>
+                <th scope="col">TrashDay</th>
+                <th scope="col">ZipCode</th>
+                <th scope="col">Full Address</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      ) : (
-        <p>no results found.</p>
-      )}
+            </thead>
+            <tbody>
+              {filterData.map((feature) => (
+                <tr key={feature.attributes.OBJECTID}>
+                  <td>{feature.attributes.StNum}</td>
+                  <td>{feature.attributes.StName}</td>
+                  <td>{feature.attributes.TrashDay}</td>
+                  <td>{feature.attributes.Zip}</td>
+                  <td>{feature.attributes.FullAddress}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <p>no results found.</p>
+        )}
+      </div>
     </div>
   );
 }
 
 export default TrashDaySearch;
+//className="table-bordered table-hover table table-bordered table-hover table-warning"
