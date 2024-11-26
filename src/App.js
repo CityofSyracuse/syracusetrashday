ee9150d754c2262f5b11d5202a565a3dbce652e4
Jason Scharf <43705075+JDScharf@users.noreply.github.com>
2024-11-26 15:15:59 -0500

    Made some changes to the trashday to get the looping of the dataset to work

--- src/App.js
+++ src/App.js
@@ -4,16 +4,6 @@ import "./App.css";
 //import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 //import {} from "";
 
-
-// import React, { useState, useEffect } from "react";
-// import axios from "axios";
-// import "./App.css";
-// //import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// //import {} from "";
-
-// import React, { useState, useEffect } from "react";
-// import axios from "axios";
-
 function TrashDaySearch() {
   const [data, setData] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
@@ -23,7 +13,7 @@ function TrashDaySearch() {
       const baseUrl = "https://services6.arcgis.com/bdPqSfflsdgFRVVM/arcgis/rest/services/Trash_Day_Schedule/FeatureServer/2/query";
       const params = {
         where: "1=1",
-        outFields: "StNum,StName,FullAddress,Zip,TrashDay",
+        outFields: "FullAddress,Zip,TrashDay",
         outSR: "4326",
         f: "json",
         resultRecordCount: 2000, // API limit
@@ -45,6 +35,7 @@ function TrashDaySearch() {
           if (features.length < 2000) break;
 
           resultOffset += 2000; // Increment offset for the next batch
+          console.log("Offset is: " + resultOffset)
         }
 
         setData(allFeatures);
@@ -58,8 +49,8 @@ function TrashDaySearch() {
 
   const filterData = data.length
     ? data.filter((feature) => {
-        if (!feature.attributes?.StName) return false;
-        const address = feature.attributes.StName.toLowerCase();
+        if (!feature.attributes?.FullAddress) return false;
+        const address = feature.attributes.FullAddress.toLowerCase();
         const date = feature.attributes?.TrashDay
           ? feature.attributes?.TrashDay.toLowerCase()
           : "";
@@ -75,7 +66,6 @@ function TrashDaySearch() {
         );
       })
     : [];
-}
 
   return (
     <div className="container.max-width">
@@ -120,95 +110,19 @@ function TrashDaySearch() {
           >
             <thead className="custom-table2 table-warning table-border-solid">
               <tr>
-                <th scope="col">House Number</th>
-                <th scope="col">Street Name</th>
+                <th scope="col">Full Address</th>
                 <th scope="col">Trash Day</th>
                 <th scope="col">Zip Code</th>
-                {/* <th scope="col">Full Address</th> */}
+
               </tr>
             </thead>
             <tbody>
               {filterData.map((feature) => (
                 <tr key={feature.attributes.OBJECTID}>
-                  <td>{feature.attributes.StNum}</td>
-                  <td>{feature.attributes.StName}</td>
+                  <td>{feature.attributes.FullAddress}</td>
                   <td>{feature.attributes.TrashDay}</td>
                   <td>{feature.attributes.Zip}</td>
-                  {/* <td>{feature.attributes.FullAddress}</td> */}
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        ) : (
-          <p className="answer">No results found.</p>
-        )}
-      </div>
-    </div>
-  );
-}
 
-export default TrashDaySearch;
-//className="table-bordered table-hover table table-bordered table-hover table-warning"
-
-
-  return (
-    <div className="container.max-width">
-      <nav className="navbar">
-        <img
-          className="syracuseLogowhite.png"
-          src="SyracuseLogowhite.png"
-          alt="Logo"
-          width="100"
-          padding-left="25"
-          padding-right="0px"
-        ></img>
-        <ul className=" nav justify-content-end">
-          <li className="nav-item"></li>
-          <li className="nav-item"></li>
-          <li className="nav-item"></li>
-          <li className="nav-item"></li>
-        </ul>
-      </nav>
-      <h1 className="title">Property Trash Day Schedule</h1>
-      <body className="Description">
-        Simply input your ZIP code or address into the search bar below to
-        receive customized details relevant to your location.
-        <p className="info">
-          *Potential disclaimer: If your specific address does not appear in
-          search results, kindly search for your street name to locate the
-          nearest address or refer to the provided map above.*
-        </p>
-      </body>
-      <div classname="container">
-        <input
-          className="searchInput"
-          type="text"
-          placeholder="Search..."
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-        {filterData.length > 0 && searchTerm ? (
-          <table
-            className="custom-table 
-           table table-bordered table-hover --bs-table-accent-bg"
-          >
-            <thead className="custom-table2 table-warning table-border-solid">
-              <tr>
-                <th scope="col">House Number</th>
-                <th scope="col">Street Name</th>
-                <th scope="col">Trash Day</th>
-                <th scope="col">Zip Code</th>
-                {/* <th scope="col">Full Address</th> */}
-              </tr>
-            </thead>
-            <tbody>
-              {filterData.map((feature) => (
-                <tr key={feature.attributes.OBJECTID}>
-                  <td>{feature.attributes.StNum}</td>
-                  <td>{feature.attributes.StName}</td>
-                  <td>{feature.attributes.TrashDay}</td>
-                  <td>{feature.attributes.Zip}</td>
-                  {/* <td>{feature.attributes.FullAddress}</td> */}
                 </tr>
               ))}
             </tbody>
